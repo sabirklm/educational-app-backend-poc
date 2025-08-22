508407e881cb219049675ea1aefd0a25c25affba
Sabir Mondal <sabir007@Sabirs-Mac-mini.local>
2025-08-22 22:54:48 +0530

    dev: simple server

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL;
 // middleware to parse JSON
 app.use(express.json());
 
+
 // basic route
 app.get('/', (req, res) => {
-  res.send('Hello, Express is running!');
+    console.log(`DB url ${DB_URL}`);
+    res.send({
+        'data': {
+            'id': "Hi "
+        }
+    });
 });
 
 // start server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
